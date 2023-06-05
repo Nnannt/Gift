05e2972a708f8247b812213e7c45c387beb3ec46
Nnannt <95611271+Nnannt@users.noreply.github.com>
2023-06-05 14:05:17 +0700

    Delete Foryou.js

--- Foryou.js
+++
@@ -1,8 +0,0 @@
-function popupku() {
-  var body = document.querySelector("body");      
-  var pWm = document.createElement("a");
-  pWm.setAttribute("href", "https://youtu.be/b1-ifLhRxNw");
-  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
-  pWm.innerHTML = "Youtube : Tekan ini";
-  body.appendChild(pWm);
-}
